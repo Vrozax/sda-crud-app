fec83dc8a647df8777bf2655d136febe40b0a4ed
Rafal Konstanty <asgy23@gmail.com>
2020-01-25 19:20:00 +0100

    Implementing /contacts endpoint for retrieving all contacts from Person.class

---
+++ src/main/java/pl/sdacademy/groupcrud/controller/PersonController.java
@@ -0,0 +1,28 @@
+package pl.sdacademy.groupcrud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.sdacademy.groupcrud.entity.Person;
+import pl.sdacademy.groupcrud.repository.PersonRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/contacts")
+public class PersonController {
+
+    @Autowired
+    PersonRepository personRepository;
+
+    @GetMapping
+    public List<Person> getContacts(){
+
+        List<Person> personList = new ArrayList<>();
+        personRepository.findAll().forEach(personList::add);
+        return personList;
+    }
+
+}
