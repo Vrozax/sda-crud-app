48b6156f0aede102b24612475f54f3e1a1e61018
Luke Hawthorne <mail@przykladowy.co.pl>
2020-01-25 13:46:00 +0100

    initial project push

---
+++ src/main/java/pl/sdacademy/groupcrud/GroupCrudApplication.java
@@ -0,0 +1,13 @@
+package pl.sdacademy.groupcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupCrudApplication.class, args);
+    }
+
+}
