1a99b0bca0369d2298dc4a94f6a52a7fca7aed8f
Karol <karolg@o2.pl>
2020-01-26 11:41:35 +0100

    Merge branch 'master' of D:\Git\sda-crud-app with conflicts.

--- src/main/java/pl/sdacademy/groupcrud/GroupCrudApplication.java
+++ src/main/java/pl/sdacademy/groupcrud/GroupCrudApplication.java
@@ -13,11 +13,5 @@ public class GroupCrudApplication {
     }
 
 
-    }
-
-
-
-
-
-
 }
+
--- src/main/java/pl/sdacademy/groupcrud/KontrolerGetById.java
+++ src/main/java/pl/sdacademy/groupcrud/KontrolerGetById.java
@@ -3,10 +3,31 @@ package pl.sdacademy.groupcrud;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import pl.sdacademy.groupcrud.entity.Person;
+import pl.sdacademy.groupcrud.repository.PersonRepository;
 
+import java.util.Optional;
+
+@RestController
 public class KontrolerGetById {
-        @GetMapping("/getbyID"/{identyfikacja})
-    public Collection<Person> getbyID(@PathVariable int identyfikacja) {
-        return ContactRepository.getbyID(id);
-//     @Query("select * from contacts where id = {identyfikacja}")}
+    PersonRepository personRepository;
+
+    public KontrolerGetById(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @GetMapping("/getbyID/{id}")
+    public Optional<Person> getbyID(@PathVariable int id) {
+        Optional<Person> result = personRepository.findPersonById(id);
+        if (!result.isPresent()) {
+            System.out.println("Nie ma takiego użytkowanika");
+        } else {
+            return result;
+        }
+        return null;
+    }
+
+}
+
 
