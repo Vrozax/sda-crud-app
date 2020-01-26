7f25eb7b99138103989b7d9e450f24ba9624d680
Vrozax <Vrozax1337@outlook.com>
2020-01-26 10:02:54 +0100

    small fixes

--- src/main/java/pl/sdacademy/groupcrud/deleteFunction/PutController.java
+++ src/main/java/pl/sdacademy/groupcrud/deleteFunction/PutController.java
@@ -9,18 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/put")
 public class PutController {
-    final Query query;
-
-    public PutController(Query query) {
-        this.query = query;
-    }
-
-    @GetMapping("/{id}/{name}/{lastname}")
-    public String putData(@PathVariable("id") Integer putId,@PathVariable("name") String name,@PathVariable("lastname") String last_name){
-        query.setFirstNameAndLastName(name,last_name,putId);
-
-
-        return "Zmieniono"+putId;
-
-    }
+//    final Query query;
+//
+//    public PutController(Query query) {
+//        this.query = query;
+//    }
+//
+//    @GetMapping("/{id}/{name}/{lastname}")
+//    public String putData(@PathVariable("id") Integer putId,@PathVariable("name") String name,@PathVariable("lastname") String last_name){
+//        query.setFirstNameAndLastName(name,last_name,putId);
+//
+//
+//        return "Zmieniono"+putId;
+//
+//    }
 }
